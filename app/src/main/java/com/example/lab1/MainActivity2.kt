8c514aa470059d2e8cd3d4a8d6802d8551522907
M2ndess <121449441+M2ndess@users.noreply.github.com>
2024-03-21 15:32:09 +0000

    At1

---
+++ app/src/main/java/com/example/lab1/MainActivity2.kt
@@ -0,0 +1,16 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun finish(view: View) {
+        finish()
+    }
+}
